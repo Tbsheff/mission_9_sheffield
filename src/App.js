ab373aee539c168c9684d0e297407a796ee185c2
Tyler Sheffield <tylersheffield427@gmail.com>
2024-03-06 08:46:12 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import basketballTeams from './CollegeBasketballTeams.json';
+
+function Team() {
+  return (
+    <div>
+      <div class="grid-container">
+        {basketballTeams.teams.map((team) => (
+          <div key={team.tid} class="card">
+            <p>{team.school}</p>
+            <p>{team.name}</p>
+            <p>{team.city}, {team.state}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Heading() {
+  return (
+    <div>
+      <h1>College Basketball Teams</h1>
+      <p>A collection of all Division 1 College Basketball Teams</p>
+    </div>
+  );
+
+}
+
+function SingleTeam() {
+  var team = basketballTeams.teams[0];
+  return (
+    <div className="grid-container">
+      <div key={team.tid} className="card" style={{ gridColumnStart: 2 }}>
+        <p>{team.school}</p>
+        <p>{team.name}</p>
+        <p>{team.city}, {team.state}</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <SingleTeam />
+      <Team />
     </div>
   );
 }
